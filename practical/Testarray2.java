ae19c0e34bd5ef24ed791aa75da8120834a5358f
kousik1997 <93372832+kousik1997@users.noreply.github.com>
2022-02-02 16:11:56 +0530

    Add files via upload

---
+++ practical/Testarray2.java
@@ -0,0 +1,11 @@
+package com.AutoBoxing;
+
+public class Testarray2 {
+	public static void main(String[] args) {
+		int a[]={1,2,3,4,5};//directly initializing and instantiating
+		for(int i=0;i<a.length;i++)//length is the property of array
+			System.out.println(a[i]);
+
+	}
+
+}
