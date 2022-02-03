c3cc66f2cbe46173108cc74a54de49b5f2731ea8
kousik1997 <93372832+kousik1997@users.noreply.github.com>
2022-02-03 19:16:55 +0530

    Add files via upload

---
+++ practical/Test11.java
@@ -0,0 +1,17 @@
+package com.AutoBoxing;
+
+public class Test11 {
+	public static void main(String[] args)
+	{
+	int arr[] = { 11, 22, 33 };
+	for (int i = 0; i < arr.length; i++)
+	System.out.print(arr[i] + " ");
+	System.out.println();
+	int arr2[] = { 11, 22, 33 };
+	//arr2[] = 
+	for (int i = 0; i < arr2.length; i++)
+	System.out.print(arr2[i] + " ");
+	}
+
+	}
+
