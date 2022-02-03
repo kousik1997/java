4f1b4846dffcf39d08d5668888f3c163db308d81
kousik1997 <93372832+kousik1997@users.noreply.github.com>
2022-02-03 15:03:43 +0530

    Add files via upload

---
+++ practical/multidimensionallength.java
@@ -0,0 +1,13 @@
+package com.AutoBoxing;
+
+public class multidimensionallength {
+	public static void main(String[] args) {
+		int[][] a = { {1,2,3},{4,5,6}, {7,8} };
+		System.out.println("length of row 1:"+a[0].length);
+		System.out.println("length of row 1:"+a[1].length);
+		System.out.println("length of row 1:"+a[2].length);
+		
+
+	}
+
+}
