ac1e27966ec2d0ffc3dbb827d9415055e4a76e18
kousik1997 <93372832+kousik1997@users.noreply.github.com>
2022-02-03 18:27:39 +0530

    Add files via upload

---
+++ practical/variable_scope.java
@@ -0,0 +1,15 @@
+package com.AutoBoxing;
+
+public class variable_scope {
+	 public static void main(String args[])
+	 { 
+	int x;
+	x = 5; 
+	{
+	int y = 6; 
+	System.out.print(x + " " + y); 
+	
+	System.out.println(x + " " + y);
+	 }
+	 }
+}
