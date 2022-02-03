152c1ca03c980242fe63d69e37cf7dc691e140c4
kousik1997 <93372832+kousik1997@users.noreply.github.com>
2022-02-03 19:37:00 +0530

    Add files via upload

---
+++ assignment/Output2.java
@@ -0,0 +1,18 @@
+package com.AutoBoxing;
+
+public class Output2 {
+	public static void main(String args[]) 
+	{
+		int x,y,z;
+		x=20;
+		z=x;
+		y=z;
+		
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		
+	 } 
+
+}
