ae7483797fe17f36dc184b8d0e76b32fb0fa4c6d
kousik1997 <93372832+kousik1997@users.noreply.github.com>
2022-02-03 19:40:19 +0530

    Add files via upload

---
+++ assignment/Output3.java
@@ -0,0 +1,12 @@
+package com.AutoBoxing;
+
+public class Output3 {
+	
+		 public static void main(String args[]) 
+		{
+		 int arr[] = {1, 2, 3, 4, 5}; 
+		for ( int i = 0; i < arr.length - 2; ++i)
+		 System.out.println(arr[i] + " "); 
+		} 
+
+}
