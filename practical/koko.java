0a7eec14681e00bbd7b89b61280223b5f957b4b1
kousik1997 <93372832+kousik1997@users.noreply.github.com>
2022-02-04 16:11:21 +0530

    Update koko.java

--- practical/koko.java
+++ practical/koko.java
@@ -7,10 +7,9 @@ public class koko {
 	public static void main(String[] args) {
 		
 		
-		String [] array = {"Print Mango","apple","grapes","papaya", "pineapple","banana","orange in descending order"};   
+		String [] array = {"Print Mango","apple","grapes","papaya", "pineapple","banana","orange"};   
 		// sorts array[] in descending order   
 		Arrays.sort(array, Collections.reverseOrder());   
 		System.out.println("Array elements in descending order: " +Arrays.toString(array));
 	}
 	}
-
