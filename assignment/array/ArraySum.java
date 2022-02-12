2219eede7d9f8120bb769e17a25fda4f8ef7c9c4
kousik1997 <93372832+kousik1997@users.noreply.github.com>
2022-02-12 19:24:47 +0530

    Add files via upload

---
+++ assignment/array/ArraySum.java
@@ -0,0 +1,14 @@
+package com.Ccc;
+
+public class ArraySum {
+public static void main(String[] args) {
+	int a[]= {10,20,30,40,50};
+	int b=0;
+	for(int i=0;i<a.length;i++) {
+		b+=a[i];
+	}
+	System.out.println("your total value is : "+b);
+	
+	
+}
+}
