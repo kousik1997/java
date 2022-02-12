bdebe77c09eacbb2b34230f74e2c454ae5d0b85d
kousik1997 <93372832+kousik1997@users.noreply.github.com>
2022-02-12 22:23:58 +0530

    Add files via upload

---
+++ assignment/array/ReverseArray.java
@@ -0,0 +1,18 @@
+package com.Ccc;
+import java.util.*;
+
+public class ReverseArray {
+	public static void main(String[] args) {
+		
+	
+	ArrayList<Integer> a=new ArrayList<Integer>();
+	a.add(10);  
+	a.add(20);  
+	a.add(30);  
+	a.add(40); 
+	a.add(50); 
+Collections.reverse(a);
+System.out.println(a);
+
+}
+}
