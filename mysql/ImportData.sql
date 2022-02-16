99844615bf8f85b36fa123dad882cc5f89ac94f9
kousik1997 <93372832+kousik1997@users.noreply.github.com>
2022-02-16 16:35:32 +0530

    Add files via upload

---
+++ mysql/ImportData.sql
@@ -0,0 +1,45 @@
+CREATE DATABASE ORG;
+SHOW DATABASES;
+USE ORG;
+CREATE TABLE Worker (
+	WORKER_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	FIRST_NAME CHAR(25),
+	LAST_NAME CHAR(25),
+	SALARY INT(15),
+	JOINING_DATE DATETIME,
+	DEPARTMENT CHAR(25)
+);
+INSERT INTO Worker 
+	(WORKER_ID, FIRST_NAME, LAST_NAME, SALARY, JOINING_DATE, DEPARTMENT) VALUES
+		(001, 'Monika', 'Arora', 100000, '14-02-20 09.00.00', 'HR'),
+		(002, 'Niharika', 'Verma', 80000, '14-06-11 09.00.00', 'Admin'),
+		(003, 'Vishal', 'Singhal', 300000, '14-02-20 09.00.00', 'HR'),
+		(004, 'Amitabh', 'Singh', 500000, '14-02-20 09.00.00', 'Admin'),
+		(005, 'Vivek', 'Bhati', 500000, '14-06-11 09.00.00', 'Admin'),
+		(006, 'Vipul', 'Diwan', 200000, '14-06-11 09.00.00', 'Account'),
+		(007, 'Satish', 'Kumar', 75000, '14-01-20 09.00.00', 'Account'),
+		(008, 'Geetika', 'Chauhan', 90000, '14-04-11 09.00.00', 'Admin');
+
+SELECT * FROM org.worker;
+USE ORG;
+create table Bonus(WORKER_REF_ID int primary key,BONUS_DATE DATETIME,BONUS_AMOUNT int);
+SELECT * FROM org.bonus;
+INSERT INTO bonus (WORKER_REF_ID,BONUS_DATE,BONUS_AMOUNT)VALUES
+(1,'2016-02-20 00:00:00',5000),
+(2,'2016-06-11 00:00:00',3000),
+(3,'2016-02-20 00:00:00',4000),
+(4,'2016-02-20 00:00:00',4500),
+(5,'2016-06-11 00:00:00',3500);
+
+USE ORG;
+create table Table_Title(WORKER_REF_ID int primary key,WORKER_TITLE varchar(50),FECTED_FROM DATETIME);
+SELECT * FROM org.Table_Title;
+INSERT INTO Table_Title(WORKER_REF_ID,WORKER_TITLE,FECTED_FROM)values
+(1,'Manager','2016-02-20 00:00:00'),
+(2,'Executive','2016-06-11 00:00:00'),
+(8,'Executive','2016-06-11 00:00:00'),
+(5,'Manager','2016-06-11 00:00:00'),
+(4,'Asst. Manager','2016-06-11 00:00:00'),
+(7,'Executive','2016-06-11 00:00:00'),
+(6,'Lead','2016-06-11 00:00:00'),
+(3,'Lead','2016-06-11 00:00:00');
